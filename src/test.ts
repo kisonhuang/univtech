d2430347bd4111862c29860cd5ceac576c4fb165
kisonhuang <916462938@qq.com>
2022-07-13 16:40:35 +0800

    修改注释

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,12 @@
-// 这个文件是karma.conf.js所需的，用于递归加载所有.spec和框架文件
+// 这个文件是karma.conf.js所需的文件，用于递归加载所有.spec和框架文件。
 
 import 'zone.js/testing';
 import {getTestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 
-// assert腻子脚本使用process时所需的
+// assert腻子脚本使用process时所需。
 // 参考：https://github.com/browserify/commonjs-assert/blob/bba838e9ba9e28edf3127ce6974624208502f6bc/internal/assert/assertion_error.js#L138
-// 需要assert腻子脚本的原因在于，timezone-mock既是一个Node.JS库，却又被浏览器所使用
+// 需要assert腻子脚本的原因在于，timezone-mock既是Node.JS库，又被浏览器所使用。
 (globalThis as any).process = {
     env: {},
 };
