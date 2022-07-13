f316fd7bc5d5050c041ec6c4051b1ab361e09291
kisonhuang <916462938@qq.com>
2022-07-13 16:07:41 +0800

    修改polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,53 +1,45 @@
 /**
- * This file includes polyfills needed by Angular and is loaded before the app.
- * You can add your own extra polyfills to this file.
+ * 这个文件包含Angular所需的，在应用程序之前加载的腻子脚本。
+ * 可以在这个文件中添加其他腻子脚本。
  *
- * This file is divided into 2 sections:
- *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
- *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
- *      file.
+ * 这个文件分成两个部分：
+ * 1、浏览器腻子脚本：ZoneJS加载之前应用，按浏览器排序的脚本。
+ * 2、应用程序导入文件：ZoneJS之后导入（import），main文件之前加载的文件。
  *
- * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
- * automatically update themselves. This includes recent versions of Safari, Chrome (including
- * Opera), Edge on the desktop, and iOS and Chrome on mobile.
+ * 当前设置针对于所谓的长青浏览器，它们会自动更新浏览器的最后一个版本。
+ * 这些浏览器包括：Safari >= 10、Chrome >= 55（包括Opera）、桌面设备的Edge >= 13、移动设备的iOS 10和Chrome。
  *
- * Learn more in https://angular.io/guide/browser-support
+ * 了解更多：https://angular.io/guide/browser-support
  */
 
 /***************************************************************************************************
- * BROWSER POLYFILLS
+ * 浏览器腻子脚本
  */
 
 /**
- * By default, zone.js will patch all possible macroTask and DomEvents
- * user can disable parts of macroTask/DomEvents patch by setting following flags
- * because those flags need to be set before `zone.js` being loaded, and webpack
- * will put import in the top of bundle, so user need to create a separate file
- * in this directory (for example: zone-flags.ts), and put the following flags
- * into that file, and then add the following code before importing zone.js.
+ * 默认情况下，zone.js会修正所有可能的宏任务和Dom事件。
+ * 通过设置以下标记，用户可以禁止修正部分宏任务或Dom事件。
+ * 因为需要在加载zone.js之前设置这些标记，并且webpack会把import放在包的顶部，
+ * 所以用户需要在这个目录中创建一个单独的文件zone-flags.ts，并在这个文件中放入以下标记，然后在`import 'zone.js';`之前添加以下代码：
  * import './zone-flags';
  *
- * The flags allowed in zone-flags.ts are listed here.
- *
- * The following flags will work for all browsers.
- *
- * (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
- * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
- * (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
- *
- *  in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
- *  with the following flag, it will bypass `zone.js` patch for IE/Edge
- *
- *  (window as any).__Zone_enable_cross_context_check = true;
- *
+ * zone-flags.ts中允许的标记如下：
+ * 1、以下标记适用于所有浏览器：
+ * (window as any).__Zone_disable_requestAnimationFrame = true;               // 禁止修正requestAnimationFrame
+ * (window as any).__Zone_disable_on_property = true;                         // 禁止修正onProperty，例如：onclick
+ * (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // 禁止修正特定事件
+ * 2、IE或Edge开发工具中，zone.js会使用以下标记来包装addEventListener，这样会绕过zone.js对IE或Edge的修正
+ * (window as any).__Zone_enable_cross_context_check = true;
  */
 
 /***************************************************************************************************
- * Zone JS is required by default for Angular itself.
+ * Angular默认需要zone.js
  */
-import 'zone.js';  // Included with Angular CLI.
+// import './zone-flags'; // zone-flags.ts中包含上述可用标记时去掉注释
+import 'zone.js';         // 与Angular CLI包含在一起
 
 
 /***************************************************************************************************
- * APPLICATION IMPORTS
+ * 应用程序导入文件
  */
+// import 'import file'   // 应用程序导入文件
