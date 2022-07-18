15eb9397bd67dc7461af8da91bb0e47e0192f053
kisonhuang <916462938@qq.com>
2022-07-18 22:54:45 +0800

    重构代码

--- src/app/base/window.service.ts
+++ src/app/base/window.service.ts
@@ -1,13 +1,7 @@
 import {InjectionToken} from '@angular/core';
 
-/**
- * Window令牌，应用于依赖注入提供器
- */
 export const WindowToken = new InjectionToken<Window>('Window');
 
-/**
- * Window提供器
- */
 export function windowProvider() {
     return window;
 }
