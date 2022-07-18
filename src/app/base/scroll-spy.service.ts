e750e1a752270a59450dd3df0c83f5d5f34890a9
kisonhuang <916462938@qq.com>
2022-07-18 16:00:18 +0800

    修改滚动监听服务

--- src/app/base/scroll-spy.service.ts
+++ src/app/base/scroll-spy.service.ts
@@ -1,74 +1,93 @@
 import {Inject, Injectable} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
+
 import {fromEvent, Observable, ReplaySubject, Subject} from 'rxjs';
 import {auditTime, distinctUntilChanged, takeUntil} from 'rxjs/operators';
 
-import {ScrollService} from 'app/shared/scroll.service';
+import {ScrollService} from './scroll.service';
 
-
+/**
+ * 滚动项
+ */
 export interface ScrollItem {
-    element: Element;
+
+    // 元素索引
     index: number;
+
+    // 滚动元素
+    element: Element;
+
 }
 
+/**
+ * 滚动监视信息
+ */
 export interface ScrollSpyInfo {
+
+    // 不同滚动项的可观察对象
     active: Observable<ScrollItem | null>;
+
+    // 停止跟踪元素组的方法
     unspy: () => void;
+
 }
 
 /*
- * Represents a "scroll-spied" element. Contains info and methods for determining whether this
- * element is the active one (i.e. whether it has been scrolled passed), based on the window's
- * scroll position.
- *
- * @prop {Element} element - The element whose position relative to the viewport is tracked.
- * @prop {number}  index   - The index of the element in the original list of element (group).
- * @prop {number}  top     - The `scrollTop` value at which this element becomes active.
+ * 表示滚动被监视的滚动元素，包含基于窗口滚动位置，用于确定元素是否滚动的信息和方法
  */
 export class ScrollSpiedElement implements ScrollItem {
+
+    // 元素顶部滚动的距离
     top = 0;
 
-    /*
-     * @constructor
-     * @param {Element} element - The element whose position relative to the viewport is tracked.
-     * @param {number}  index   - The index of the element in the original list of element (group).
+    /**
+     * 构造函数，创建滚动元素
+     *
+     * @param element 相对视窗位置被跟踪的滚动元素
+     * @param index 原元素（组）列表中元素的索引
      */
-    constructor(public readonly element: Element, public readonly index: number) {
+    constructor(public readonly element: Element,
+                public readonly index: number) {
+
     }
 
-    /*
-     * @method
-     * Caclulate the `top` value, i.e. the value of the `scrollTop` property at which this element
-     * becomes active. The current implementation assumes that window is the scroll-container.
+    /**
+     * 计算元素顶部滚动的距离
      *
-     * @param {number} scrollTop - How much is window currently scrolled (vertically).
-     * @param {number} topOffset - The distance from the top at which the element becomes active.
+     * @param scrollTop 窗口垂直滚动的距离
+     * @param topOffset 元素的顶部偏移量
      */
     calculateTop(scrollTop: number, topOffset: number) {
         this.top = scrollTop + this.element.getBoundingClientRect().top - topOffset;
     }
+
 }
 
 /*
- * Represents a group of "scroll-spied" elements. Contains info and methods for efficiently
- * determining which element should be considered "active", i.e. which element has been scrolled
- * passed the top of the viewport.
+ * 表示滚动被监视的滚动元素组
  *
- * @prop {Observable<ScrollItem | null>} activeScrollItem - An observable that emits ScrollItem
- *     elements (containing the HTML element and its original index) identifying the latest "active"
- *     element from a list of elements.
+ *
+ * @prop {Observable<ScrollItem | null>} activeScrollItem -
+ *
+ * An observable that emits ScrollItem elements
+ * (containing the HTML element and its original index)
+ * identifying the latest "active" element from a list of elements.
  */
 export class ScrollSpiedElementGroup {
+
+    // 最后一个滚动元素
     activeScrollItem: ReplaySubject<ScrollItem | null> = new ReplaySubject(1);
+
+    // 滚动被监视的滚动元素数组
     private spiedElements: ScrollSpiedElement[];
 
-    /*
-     * @constructor
-     * @param {Element[]} elements - A list of elements whose position relative to the viewport will
-     *     be tracked, in order to determine which one is "active" at any given moment.
+    /**
+     * 构造函数，创建滚动元素组
+     *
+     * @param elements 相对视窗位置被跟踪的滚动元素数组
      */
     constructor(elements: Element[]) {
-        this.spiedElements = elements.map((elem, i) => new ScrollSpiedElement(elem, i));
+        this.spiedElements = elements.map((element, index) => new ScrollSpiedElement(element, index));
     }
 
     /*
@@ -81,21 +100,24 @@ export class ScrollSpiedElementGroup {
      * @param {number} topOffset - The distance from the top at which the element becomes active.
      */
     calibrate(scrollTop: number, topOffset: number) {
-        this.spiedElements.forEach(spiedElem => spiedElem.calculateTop(scrollTop, topOffset));
-        this.spiedElements.sort((a, b) => b.top - a.top);   // Sort in descending `top` order.
+        this.spiedElements.forEach(element => element.calculateTop(scrollTop, topOffset));
+        this.spiedElements.sort((first, second) => second.top - first.top);
     }
 
     /*
-     * @method
-     * Determine which element is the currently active one, i.e. the lower-most element that is
-     * scrolled passed the top of the viewport (taking offsets into account) and emit it on
-     * `activeScrollItem`.
-     * If no element can be considered active, `null` is emitted instead.
-     * If window is scrolled all the way to the bottom, then the lower-most element is considered
-     * active even if it not scrolled passed the top of the viewport.
+     * Determine which element is the currently active one,
+     * i.e. the lower-most element that is scrolled passed the top of the viewport (taking offsets into account) and emit it on `activeScrollItem`.
      *
-     * @param {number} scrollTop    - How much is window currently scrolled (vertically).
-     * @param {number} maxScrollTop - The maximum possible `scrollTop` (based on the viewport size).
+     * If window is scrolled all the way to the bottom, then the lower-most element is considered active
+     * even if it not scrolled passed the top of the viewport.
+     *
+     */
+
+    /**
+     * 确定当前活动的元素，
+     *
+     * @param scrollTop 窗口垂直滚动的高度
+     * @param maxScrollTop 基于视窗大小的最大滚动高度
      */
     onScroll(scrollTop: number, maxScrollTop: number) {
         let activeItem: ScrollItem | undefined;
@@ -103,42 +125,86 @@ export class ScrollSpiedElementGroup {
         if (scrollTop + 1 >= maxScrollTop) {
             activeItem = this.spiedElements[0];
         } else {
-            this.spiedElements.some(spiedElem => {
-                if (spiedElem.top <= scrollTop) {
-                    activeItem = spiedElem;
+            this.spiedElements.some(element => {
+                if (element.top <= scrollTop) {
+                    activeItem = element;
                     return true;
                 }
                 return false;
             });
         }
 
+        // 没有活动元素时，发送null
         this.activeScrollItem.next(activeItem || null);
     }
 }
 
 @Injectable()
 export class ScrollSpyService {
-    private spiedElementGroups: ScrollSpiedElementGroup[] = [];
-    private onStopListening = new Subject<void>();
-    private resizeEvents = fromEvent(window, 'resize').pipe(auditTime(300), takeUntil(this.onStopListening));
-    private scrollEvents = fromEvent(window, 'scroll').pipe(auditTime(10), takeUntil(this.onStopListening));
-    private lastContentHeight: number;
-    private lastMaxScrollTop: number;
 
-    constructor(@Inject(DOCUMENT) private doc: any, private scrollService: ScrollService) {
+    //
+    private spiedElementGroups: ScrollSpiedElementGroup[] = [];
+
+    //
+    private onStopListening = new Subject<void>();
+
+    // resize事件
+    private resizeEvents = fromEvent(window, 'resize').pipe(auditTime(300), takeUntil(this.onStopListening));
+
+    // scroll事件
+    private scrollEvents = fromEvent(window, 'scroll').pipe(auditTime(10), takeUntil(this.onStopListening));
+
+    // 最后的内容高度
+    private lastContentHeight = 0;
+
+    // 最后的最大滚动高度
+    private lastMaxScrollTop = 0;
+
+    /**
+     * 构造函数，创建滚动监听服务
+     *
+     * @param document 文档对象
+     * @param scrollService 滚动服务
+     */
+    constructor(@Inject(DOCUMENT) private document: any,
+                private scrollService: ScrollService) {
+
     }
 
-    /*
-     * @method
-     * Start tracking a group of elements and emitting active elements; i.e. elements that are
-     * currently visible in the viewport. If there was no other group being spied, start listening for
-     * `resize` and `scroll` events.
+    /**
+     * 获取内容高度
+     */
+    private getContentHeight() {
+        return this.document.body.scrollHeight || Number.MAX_SAFE_INTEGER;
+    }
+
+    /**
+     * 获取滚动高度
+     */
+    private getScrollTop() {
+        return window && window.pageYOffset || 0;
+    }
+
+    /**
+     * 获取顶部偏移量
+     */
+    private getTopOffset() {
+        return this.scrollService.topOffset + 50;
+    }
+
+    /**
+     * 获取视窗高度
+     */
+    private getViewportHeight() {
+        return this.document.body.clientHeight || 0;
+    }
+
+    /**
+     * 跟踪元素并发送活动元素，例如，视窗中当前可见的元素。
+     * 没有待监视元素时，监听resize和scroll事件。
      *
-     * @param {Element[]} elements - A list of elements to track.
-     *
-     * @return {ScrollSpyInfo} - An object containing the following properties:
-     *     - `active`: An observable of distinct ScrollItems.
-     *     - `unspy`: A method to stop tracking this group of elements.
+     * @param elements 待跟踪元素
+     * @return 滚动监听信息
      */
     spyOn(elements: Element[]): ScrollSpyInfo {
         if (!this.spiedElementGroups.length) {
@@ -163,26 +229,8 @@ export class ScrollSpyService {
         };
     }
 
-    private getContentHeight() {
-        return this.doc.body.scrollHeight || Number.MAX_SAFE_INTEGER;
-    }
-
-    private getScrollTop() {
-        return window && window.pageYOffset || 0;
-    }
-
-    private getTopOffset() {
-        return this.scrollService.topOffset + 50;
-    }
-
-    private getViewportHeight() {
-        return this.doc.body.clientHeight || 0;
-    }
-
-    /*
-     * @method
-     * The size of the window has changed. Re-calculate all affected values,
-     * so that active elements can be determined efficiently on scroll.
+    /**
+     * 窗口大小发生改变，重新计算所有受影响的值，这样才能在滚动时有效地确定活动元素
      */
     private onResize() {
         const contentHeight = this.getContentHeight();
@@ -192,7 +240,6 @@ export class ScrollSpyService {
 
         this.lastContentHeight = contentHeight;
         this.lastMaxScrollTop = contentHeight - viewportHeight;
-
         this.spiedElementGroups.forEach(group => group.calibrate(scrollTop, topOffset));
     }
 
@@ -213,19 +260,22 @@ export class ScrollSpyService {
         this.spiedElementGroups.forEach(group => group.onScroll(scrollTop, maxScrollTop));
     }
 
-    /*
-     * @method
-     * Stop tracking this group of elements and emitting active elements. If there is no other group
-     * being spied, stop listening for `resize` or `scroll` events.
+    /**
+     * 停止跟踪元素组
      *
-     * @param {ScrollSpiedElementGroup} spiedGroup - The group to stop tracking.
+     * @param spiedElementGroup 被跟踪元素组
      */
-    private unspy(spiedGroup: ScrollSpiedElementGroup) {
-        spiedGroup.activeScrollItem.complete();
-        this.spiedElementGroups = this.spiedElementGroups.filter(group => group !== spiedGroup);
+    private unspy(spiedElementGroup: ScrollSpiedElementGroup) {
+        // 停止发送活动元素
+        spiedElementGroup.activeScrollItem.complete();
 
+        // 删除被监视的元素组
+        this.spiedElementGroups = this.spiedElementGroups.filter(group => group !== spiedElementGroup);
+
+        // 不存在被监视的元素时，停止监听resize和scroll事件s
         if (!this.spiedElementGroups.length) {
             this.onStopListening.next();
         }
     }
+
 }
