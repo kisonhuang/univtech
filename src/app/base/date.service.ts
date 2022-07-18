316482513c4632c78e88785bb3b18ceb37e15e4c
kisonhuang <916462938@qq.com>
2022-07-18 18:18:54 +0800

    重构日期服务

--- src/app/base/date.service.ts
+++ src/app/base/date.service.ts
@@ -1,13 +1,7 @@
 import {InjectionToken} from '@angular/core';
 
-/**
- * 当前日期令牌，用于依赖注入提供器
- */
 export const CurrentDateToken = new InjectionToken('CurrentDate');
 
-/**
- * 当前日期提供器
- */
 export function currentDateProvider() {
     return new Date();
 }
