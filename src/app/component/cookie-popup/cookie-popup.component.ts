88821246018ca6b2a3d75d4bda3d100cb850e350
kisonhuang <916462938@qq.com>
2022-07-19 08:33:34 +0800

    重构cookie-popup组件

---
+++ src/app/component/cookie-popup/cookie-popup.component.ts
@@ -0,0 +1,24 @@
+import {Component, Inject} from '@angular/core';
+
+import {LocalStorageToken} from '../../base/storage.service';
+
+export const StorageKeyAcceptCookie = 'sk-accept-cookie';
+
+@Component({
+    selector: 'univ-cookie-popup',
+    templateUrl: './cookie-popup.component.html',
+})
+export class CookiePopupComponent {
+
+    hasAcceptedCookie: boolean;
+
+    constructor(@Inject(LocalStorageToken) private storage: Storage) {
+        this.hasAcceptedCookie = this.storage.getItem(StorageKeyAcceptCookie) === 'true';
+    }
+
+    acceptCookie() {
+        this.storage.setItem(StorageKeyAcceptCookie, 'true');
+        this.hasAcceptedCookie = true;
+    }
+
+}
