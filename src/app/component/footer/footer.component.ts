43b3570984da397098fb9b5bfb957aaa0dd0f51c
kisonhuang <916462938@qq.com>
2022-07-19 14:32:12 +0800

    重构页脚组件

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
-import { NavigationNode, VersionInfo } from 'app/navigation/navigation.service';
+import {NavigationNode, VersionInfo} from '../navigation/navigation.service';
 
 @Component({
-  selector: 'aio-footer',
-  templateUrl: 'footer.component.html'
+    selector: 'univ-footer',
+    templateUrl: 'footer.component.html'
 })
 export class FooterComponent {
-  @Input() nodes: NavigationNode[];
-  @Input() versionInfo: VersionInfo;
+
+    @Input() nodes?: NavigationNode[];
+
 }
