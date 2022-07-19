fd82043f6480d84cc3e1562de2e89a6ff0eb5702
kisonhuang <916462938@qq.com>
2022-07-19 22:27:38 +0800

    重命名

--- src/app/component/code/code-pretty.service.ts
+++ src/app/component/code/code-pretty.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 
-import {from, Observable} from 'rxjs';
-import {first, map, share} from 'rxjs/operators';
+import {Observable, from} from 'rxjs';
+import {share, map, first} from 'rxjs/operators';
 
 import {htmlFromStringKnownToSatisfyTypeContract} from 'safevalues/unsafe/reviewed';
 
@@ -10,15 +10,15 @@ import {LogService} from '../../base/log.service';
 // 代码美化对象
 type PrettyPrintOne = (code: TrustedHTML, language?: string, linenum?: number | boolean) => string;
 
-// 代码打印服务，包装prettify.js库
+// 代码美化服务，包装prettify.js库
 @Injectable()
-export class CodePrintService {
+export class CodePrettyService {
 
     // 代码美化对象的Observable
     private prettyPrintOne: Observable<PrettyPrintOne>;
 
     /**
-     * 构造函数，创建代码打印服务
+     * 构造函数，创建代码美化服务
      *
      * @param logService 日志服务
      */
@@ -32,8 +32,8 @@ export class CodePrintService {
      * @return Promise<PrettyPrintOne> 代码美化对象的Promise
      */
     private getPrettyPrintOne(): Promise<PrettyPrintOne> {
-        const print = (window as any).prettyPrintOne;
-        return print ? Promise.resolve(print) : import('assets/js/prettify.js' as any).then(
+        const pretty = (window as any).prettyPrintOne;
+        return pretty ? Promise.resolve(pretty) : import('assets/js/prettify.js' as any).then(
             () => (window as any).prettyPrintOne,
             error => {
                 const message = `无法获取prettify.js：${error.message}`;
@@ -49,14 +49,14 @@ export class CodePrintService {
      *
      * @param code 代码片段
      * @param language 代码语言，例如：html、javascript、typescript
-     * @param linenum 是否显示行号，false：不显示行号，true：显示行号，number：从给定数字开始显示行号
+     * @param linenum 是否显示行号，number：从给定数字开始显示行号，true：显示行号，false：不显示行号
      * @return Observable<string> 已格式化代码的Observable
      */
     formatCode(code: TrustedHTML, language?: string, linenum?: number | boolean) {
         return this.prettyPrintOne.pipe(
-            map(print => {
+            map(pretty => {
                 try {
-                    return htmlFromStringKnownToSatisfyTypeContract(print(code, language, linenum), 'prettify.js修改时已信任内联HTML');
+                    return htmlFromStringKnownToSatisfyTypeContract(pretty(code, language, linenum), 'prettify.js修改时已信任内联HTML');
                 } catch (error) {
                     const message = `无法格式化代码：'${code.toString().slice(0, 50)}...'`;
                     console.error(message, error);
