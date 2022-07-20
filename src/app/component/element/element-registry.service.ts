fd09fb635d87bfbf3aa4f4f389c7a07392c32baf
kisonhuang <916462938@qq.com>
2022-07-20 17:16:34 +0800

    重构元素注册表

--- src/app/component/element/element-registry.service.ts
+++ src/app/component/element/element-registry.service.ts
@@ -1,16 +1,19 @@
 import {InjectionToken, Type} from '@angular/core';
 import {LoadChildrenCallback} from '@angular/router';
 
+// 元素模块接口，由声明元素组件的模块实现
+export interface ElementModule {
+    elementComponent: Type<any>; // 元素组件
+}
+
+// 元素模块映射令牌
+export const ElementModuleToken = new InjectionToken<Map<string, LoadChildrenCallback>>('ElementModuleMap');
+
+// 元素模块映射
+export const ElementModuleMap = new Map<string, LoadChildrenCallback>();
+
 // 元素模块路由
 export const ElementModuleRoutes = [
-    // {
-    //     selector: 'aio-file-not-found-search',
-    //     loadChildren: () => import('./search/file-not-found-search.module').then(m => m.FileNotFoundSearchModule)
-    // },
-    // {
-    //     selector: 'aio-toc',
-    //     loadChildren: () => import('./toc/toc.module').then(m => m.TocModule)
-    // },
     {
         selector: 'univ-code-example',
         loadChildren: () => import('../code/code-example.module').then(module => module.CodeExampleModule),
@@ -19,22 +22,15 @@ export const ElementModuleRoutes = [
         selector: 'univ-code-tab',
         loadChildren: () => import('../code/code-tab.module').then(module => module.CodeTabModule),
     },
+    // {
+    //     selector: 'aio-file-not-found-search',
+    //     loadChildren: () => import('./search/file-not-found-search.module').then(m => m.FileNotFoundSearchModule)
+    // },
+    // {
+    //     selector: 'aio-toc',
+    //     loadChildren: () => import('./toc/toc.module').then(m => m.TocModule)
+    // },
 ];
-
-/**
- * 元素模块接口，由声明元素组件的模块实现
- */
-export interface ElementModule {
-
-    elementComponent: Type<any>;
-
-}
-
-// 元素模块映射令牌
-export const ElementModuleToken = new InjectionToken<Map<string, LoadChildrenCallback>>('ElementModuleMap');
-
-// 元素模块映射
-export const ElementModuleMap = new Map<string, LoadChildrenCallback>();
 
 // 把元素模块路由转换为元素选择器与延时加载模块之间的映射
 ElementModuleRoutes.forEach(route => {
