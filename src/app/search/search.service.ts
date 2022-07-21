267be4b53e2e3dd32235621421cbe7bca82c87e7
kisonhuang <916462938@qq.com>
2022-07-21 17:28:58 +0800

    重构搜索服务

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -1,56 +1,66 @@
-import {NgZone, Injectable} from '@angular/core';
+import {Injectable, NgZone} from '@angular/core';
+
 import {ConnectableObservable, Observable, race, ReplaySubject, timer} from 'rxjs';
 import {concatMap, first, publishReplay} from 'rxjs/operators';
+
 import {WebWorkerClient} from '../base/worker.service';
 import {SearchResults} from './search.model';
 
+/**
+ * 搜索服务
+ */
 @Injectable()
 export class SearchService {
-    private ready: Observable<boolean>;
-    private searchesSubject = new ReplaySubject<string>(1);
-    private worker: WebWorkerClient;
 
-    constructor(private zone: NgZone) {
+    // 初始化完成的可观察对象
+    private initialized: Observable<boolean>;
+
+    // 搜索主题
+    private searchSubject = new ReplaySubject<string>(1);
+
+    // WebWorker客户端
+    private webWorkerClient: WebWorkerClient;
+
+    /**
+     * 构造函数，创建搜索服务
+     *
+     * @param ngZone 在Angular区域内外执行任务的服务
+     */
+    constructor(private ngZone: NgZone) {
+
     }
 
     /**
-     * Initialize the search engine. We offer an `initDelay` to prevent the search initialisation from delaying the
-     * initial rendering of the web page. Triggering a search will override this delay and cause the index to be
-     * loaded immediately.
+     * 初始化WebWorker
      *
-     * @param initDelay the number of milliseconds to wait before we load the WebWorker and generate the search index
+     * @param initDelay 初始化延时（毫秒）
      */
-    initWorker(initDelay: number) {
-        // Wait for the initDelay or the first search
-        const ready = this.ready = race<any>(
+    initWebWorker(initDelay: number) {
+        const initialized = this.initialized = race<any>(
             timer(initDelay),
-            this.searchesSubject.asObservable().pipe(first()),
-        )
-            .pipe(
-                concatMap(() => {
-                    // Create the worker and load the index
-                    const worker = new Worker(new URL('./search.worker', import.meta.url), {type: 'module'});
-                    this.worker = WebWorkerClient.create(worker, this.zone);
-                    return this.worker.sendMessage<boolean>('load-index');
-                }),
-                publishReplay(1),
-            );
+            this.searchSubject.asObservable().pipe(first()),
+        ).pipe(
+            concatMap(() => {
+                const worker = new Worker(new URL('./search.worker', import.meta.url), {type: 'module'});
+                this.webWorkerClient = WebWorkerClient.create(worker, this.ngZone);
+                return this.webWorkerClient.sendMessage<boolean>('load-index');
+            }),
+            publishReplay(1),
+        );
 
-        // Connect to the observable to kick off the timer
-        (ready as ConnectableObservable<boolean>).connect();
-        return ready;
+        (initialized as ConnectableObservable<boolean>).connect();
+        return initialized;
     }
 
     /**
-     * Search the index using the given query and emit results on the observable that is returned.
+     * 搜索索引
      *
-     * @param query The query to run against the index.
-     * @returns an observable collection of search results
+     * @param queryText 查询文本
+     * @return Observable<SearchResults> 搜索结果的可观察对象
      */
-    search(query: string): Observable<SearchResults> {
-        // Trigger the searches subject to override the init delay timer
-        this.searchesSubject.next(query);
-        // Once the index has loaded, switch to listening to the searches coming in.
-        return this.ready.pipe(concatMap(() => this.worker.sendMessage<SearchResults>('query-index', query)));
+    searchIndex(queryText: string): Observable<SearchResults> {
+        this.searchSubject.next(queryText);
+        return this.initialized.pipe(concatMap(() => this.webWorkerClient.sendMessage<SearchResults>('query-index', queryText)));
     }
+
 }
