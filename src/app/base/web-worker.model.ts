06cd17f27760b0d1ce40216af628c0c45faacd4d
kisonhuang <916462938@qq.com>
2022-07-22 09:08:49 +0800

    添加注释

--- src/app/base/web-worker.model.ts
+++ src/app/base/web-worker.model.ts
@@ -1,6 +1,6 @@
 // WebWorker消息
 export interface WebWorkerMessage {
-    id?: number;
-    type: string;
-    payload: any;
+    type: string; // 消息类型
+    id?: number;  // 消息id
+    payload: any; // 有效负载
 }
