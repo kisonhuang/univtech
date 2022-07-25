49e855bad768cecf97cc1603b399356c4e038045
kisonhuang <916462938@qq.com>
2022-07-25 15:26:09 +0800

    添加注释

--- src/app/base/error.service.ts
+++ src/app/base/error.service.ts
@@ -1,15 +1,29 @@
-import {ErrorHandler, Inject, Injectable, VERSION} from '@angular/core';
+import {Inject, Injectable, ErrorHandler, VERSION} from '@angular/core';
 
 import {windowToken} from './window.service';
 
+/**
+ * 错误服务，扩展默认的错误处理器，向谷歌分析服务报告错误。
+ * Angular应用程序之外的错误也可以通过window.onerror来处理。
+ */
 @Injectable()
 export class ErrorService extends ErrorHandler {
 
+    /**
+     * 构造函数，创建错误服务
+     *
+     * @param window Window对象
+     */
     constructor(@Inject(windowToken) private window: Window) {
         super();
     }
 
-    override handleError(error: any) {
+    /**
+     * 处理错误，把错误信息发送到谷歌分析服务
+     *
+     * @param error 错误对象
+     */
+    override handleError(error: any): void {
         const versionedError = this.addVersionPrefix(error);
         try {
             super.handleError(versionedError);
@@ -19,20 +33,31 @@ export class ErrorService extends ErrorHandler {
         this.reportError(versionedError);
     }
 
+    /**
+     * 在错误信息前面添加版本信息
+     *
+     * @param error 错误对象
+     * @return T 错误对象
+     */
     private addVersionPrefix<T>(error: T): T {
-        const prefix = `[v${VERSION.full}] `;
+        const versionPrefix = `[v${VERSION.full}] `;
         if (error instanceof Error) {
             const oldMessage = error.message;
-            const newMessage = prefix + oldMessage;
+            const newMessage = versionPrefix + oldMessage;
             error.message = newMessage;
             error.stack = error.stack?.replace(oldMessage, newMessage);
         } else if (typeof error === 'string') {
-            error = prefix + error as unknown as T;
+            error = versionPrefix + error as unknown as T;
         }
         return error;
     }
 
-    private reportError(error: unknown) {
+    /**
+     * 报告错误
+     *
+     * @param error 错误对象
+     */
+    private reportError(error: unknown): void {
         if (this.window.onerror) {
             if (error instanceof Error) {
                 this.window.onerror(error.message, undefined, undefined, undefined, error);
@@ -41,7 +66,7 @@ export class ErrorService extends ErrorHandler {
                     try {
                         error = JSON.stringify(error);
                     } catch {
-
+                        // 忽略错误
                     }
                 }
                 this.window.onerror(`${error}`);
