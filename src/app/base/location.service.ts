2dcfd156e0bb97e8f150b76f6aacf4ba3b4cd70e
kisonhuang <916462938@qq.com>
2022-07-26 09:47:38 +0800

    重构位置服务

--- src/app/base/location.service.ts
+++ src/app/base/location.service.ts
@@ -10,7 +10,8 @@ import {ScrollService} from './scroll.service';
 @Injectable()
 export class LocationService {
 
-    private readonly urlParser = document.createElement('a');
+    // 当前链接元素
+    private readonly anchorElement = document.createElement('a');
 
     private urlSubject = new ReplaySubject<string>(1);
 
@@ -99,7 +100,7 @@ export class LocationService {
      * 如果链接指向将要显示的文档，那么使用gotoUrl进行导航，并告诉浏览器不需要处理链接的点击事件。
      * 大多数应用程序可能都会在链接上添加的LinkDirective中这么做，但是这个应用程序存在一个特殊情况，
      * DocViewerComponent显示的是不能包含指令的半静态内容，所以内容中的所有链接都不能使用LinkDirective。
-     * 因此在AppComponent中添加了一个应用程序中所有元素的点击事件处理器，用于捕获DocViewerComponent内外所有链接的点击事件。
+     * 因此AppComponent中添加了一个所有元素的点击事件处理器，用于捕获DocViewerComponent内外所有链接的点击事件。
      *
      * @param anchorElement 点击的链接
      * @param mouseClicks 鼠标的点击次数，0：点击鼠标左键或没有点击鼠标
@@ -113,7 +114,7 @@ export class LocationService {
             return true;
         }
 
-        // 链接中存在target属性，并且target属性的值不是_self时，让浏览器处理链接的点击事件
+        // 链接中target属性的值不是_self时，让浏览器处理链接的点击事件
         const anchorTarget = anchorElement.target;
         if (anchorTarget && anchorTarget !== '_self') {
             return true;
@@ -124,20 +125,20 @@ export class LocationService {
             return true;
         }
 
-        // 修复页内链接，页内链接应该指向页内片段，相对于根路径`/`来进行解析，因为基础路径已被设置成为`/`
+        // 修复页内链接，页内链接应该指向页内片段，因为基础路径已经被设置为`/`，所以相对于根路径`/`来进行路径解析
         // 参考：https://developer.mozilla.org/en-US/docs/Web/HTML/Element/base#in-page_anchors
         const isInPageAnchor = anchorElement.getAttribute('href')?.startsWith('#') ?? false;
-        const realPathname = isInPageAnchor ? this.location.path() : anchorElement.pathname;
-        const relativeUrl = realPathname + anchorElement.search + anchorElement.hash;
-        this.urlParser.href = relativeUrl;
+        const pathname = isInPageAnchor ? this.location.path() : anchorElement.pathname;
+        const anchorHref = pathname + anchorElement.search + anchorElement.hash;
+        this.anchorElement.href = anchorHref;
 
         // 链接是外部链接，或者链接中存在扩展名时，让浏览器处理链接的点击事件
-        if ((!isInPageAnchor && anchorElement.href !== this.urlParser.href) || !/\/[^/.]*$/.test(anchorElement.pathname)) {
+        if ((!isInPageAnchor && anchorElement.href !== this.anchorElement.href) || !/\/[^/.]*$/.test(anchorElement.pathname)) {
             return true;
         }
 
         // 使用gotoUrl进行导航，浏览器不需要处理链接的点击事件
-        this.gotoUrl(relativeUrl);
+        this.gotoUrl(anchorHref);
         return false;
     }
 
