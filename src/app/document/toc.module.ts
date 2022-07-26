74172e4678a7abc77ca6a029d0099f171e50d5bf
kisonhuang <916462938@qq.com>
2022-07-26 20:25:10 +0800

    格式化代码，修改错误代码

--- src/app/document/toc.module.ts
+++ src/app/document/toc.module.ts
@@ -1,13 +1,16 @@
-import { NgModule, Type } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { WithCustomElementComponent } from '../element-registry';
-import { TocComponent } from './toc.component';
+import {NgModule, Type} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+
+import {ElementComponentModule} from '../element/element-registry.service';
+import {TocComponent} from './toc.component';
 
 @NgModule({
-  imports: [ CommonModule, MatIconModule ],
-  declarations: [ TocComponent ],
+    imports: [CommonModule, MatIconModule],
+    declarations: [TocComponent],
 })
-export class TocModule implements WithCustomElementComponent {
-  customElementComponent: Type<any> = TocComponent;
+export class TocModule implements ElementComponentModule {
+
+    elementComponent: Type<any> = TocComponent;
+
 }
