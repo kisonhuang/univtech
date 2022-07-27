2e6eef35e8a60323c10b72e73a8b759f4ed2de4b
kisonhuang <916462938@qq.com>
2022-07-27 08:46:37 +0800

    重构文档服务

--- src/app/document/doc.service.ts
+++ src/app/document/doc.service.ts
@@ -12,95 +12,134 @@ import {LocationService} from '../base/location.service';
 
 import {DocSafe, DocUnsafe} from './doc.model';
 
-export const UrlPrefixContent = 'content/';
+export const urlPrefixContent = 'content/';
 
-export const UrlPrefixDocs = UrlPrefixContent + 'docs/';
+export const urlPrefixDocs = urlPrefixContent + 'docs/';
 
-export const DocNotFoundId = 'file-not-found';
+export const docNotFoundId = 'doc-not-found';
 
-export const DocFetchErrorId = 'doc-fetch-error';
+export const docErrorId = 'doc-error';
 
-const docFetchErrorContent = (path: string) => htmlFromStringKnownToSatisfyTypeContract(`
+const docErrorContent = (docId: string) => htmlFromStringKnownToSatisfyTypeContract(`
     <div class="nf-container l-flex-wrap flex-center">
         <div class="nf-icon material-icons">error_outline</div>
         <div class="nf-response l-flex-wrap center">
-            <h1 class="no-toc">请求文档失败</h1>
-            <p>无法获取${htmlEscape(path)}页面，请检查连接，稍后再试。</p>
+            <h1 class="no-toc">文档获取失败</h1>
+            <p>无法获取文档，请检查连接稍后再试。${htmlEscape(docId)}</p>
         </div>
-    </div>`, ''
+    </div>`, '^'
 );
 
 @Injectable()
 export class DocService {
 
-    private docMap = new Map<string, Observable<DocSafe>>();
+    // 文档主题映射，key：文档id；value：文档主题
+    private docObservableMap = new Map<string, Observable<DocSafe>>();
 
-    currentDocument: Observable<DocSafe>;
+    // 当前文档主题
+    currentDocObservable: Observable<DocSafe>;
 
+    /**
+     * 构造函数，创建文档服务
+     *
+     * @param httpClient HTTP客户端
+     * @param logService 日志服务
+     * @param locationService 地址服务
+     */
     constructor(private httpClient: HttpClient,
                 private logService: LogService,
-                location: LocationService) {
-        this.currentDocument = location.currentPathObservable.pipe(switchMap(path => this.getDocument(path)));
+                locationService: LocationService) {
+        this.currentDocObservable = locationService.currentPathObservable.pipe(switchMap(currentPath => this.getDoc(currentPath)));
     }
 
-    private getDocument(url: string) {
-        const id = url || 'index';
-        this.logService.log('获取文档：', id);
-        if (!this.docMap.has(id)) {
-            this.docMap.set(id, this.fetchDocument(id));
+    /**
+     * 获取文档
+     *
+     * @param docPath 文档路径
+     * @return 文档主题
+     */
+    private getDoc(docPath: string): Observable<DocSafe> {
+        const docId = docPath || 'index';
+        this.logService.log('获取文档：', docId);
+        if (!this.docObservableMap.has(docId)) {
+            this.docObservableMap.set(docId, this.getDocContent(docId));
         }
-        return this.docMap.get(id) as Observable<DocSafe>;
+        return this.docObservableMap.get(docId) as Observable<DocSafe>;
     }
 
-    private fetchDocument(id: string): Observable<DocSafe> {
-        const requestPath = `${UrlPrefixDocs}${encodeToLowerCase(id)}.json`;
-        const subject = new AsyncSubject<DocSafe>();
+    /**
+     * 获取文档内容
+     *
+     * @param docId 文档id
+     * @return 文档主题
+     */
+    private getDocContent(docId: string): Observable<DocSafe> {
+        const docPath = `${urlPrefixDocs}${convertDocId(docId)}.json`;
+        const docSubject = new AsyncSubject<DocSafe>();
 
-        this.logService.log('获取文档：', requestPath);
-        this.httpClient.get<DocUnsafe>(requestPath, {responseType: 'json'})
+        this.logService.log('获取文档：', docPath);
+        this.httpClient.get<DocUnsafe>(docPath, {responseType: 'json'})
             .pipe(
-                tap(data => {
-                    if (!data || typeof data !== 'object') {
-                        this.logService.log('接受到无效数据：', data);
-                        throw Error('无效数据');
+                tap(doc => {
+                    if (!doc || typeof doc !== 'object') {
+                        this.logService.log('文档数据无效：', doc);
+                        throw Error('文档数据无效');
                     }
                 }),
                 map((doc: DocUnsafe) => ({
                     id: doc.id,
                     content: doc.content === null ? null : htmlFromStringKnownToSatisfyTypeContract(doc.content, '^')
                 })),
-                catchError((error: HttpErrorResponse) =>
-                    error.status === 404 ? this.getFileNotFoundDoc(id) : this.getErrorDoc(id, error)
+                catchError((response: HttpErrorResponse) =>
+                    response.status === 404 ? this.getDocContentNotFound(docId) : this.getDocContentError(docId, response)
                 ),
             )
-            .subscribe(subject);
+            .subscribe(docSubject);
 
-        return subject.asObservable();
+        return docSubject.asObservable();
     }
 
-    private getFileNotFoundDoc(id: string): Observable<DocSafe> {
-        if (DocNotFoundId !== id) {
-            this.logService.error(new Error(`文档'${id}'未找到`));
-            return this.getDocument(DocNotFoundId);
+    /**
+     * 获取文档未找到时的文档内容
+     * @param docId 文档id
+     * @return 文档主题
+     */
+    private getDocContentNotFound(docId: string): Observable<DocSafe> {
+        if (docId !== docNotFoundId) {
+            this.logService.error(new Error(`文档未找到，文档路径：${docId}`));
+            return this.getDoc(docNotFoundId);
         } else {
             return of({
-                id: DocNotFoundId,
+                id: docNotFoundId,
                 content: htmlEscape('文档未找到')
             });
         }
     }
 
-    private getErrorDoc(id: string, error: HttpErrorResponse): Observable<DocSafe> {
-        this.logService.error(new Error(`文档'${id}'获取错误：(${error.message})`));
-        this.docMap.delete(id);
+    /**
+     * 获取文档错误时的文档内容
+     *
+     * @param docId 文档id
+     * @param response 错误响应
+     * @return 文档主题
+     */
+    private getDocContentError(docId: string, response: HttpErrorResponse): Observable<DocSafe> {
+        this.logService.error(new Error(`文档获取错误，文档路径：${docId}；错误信息：${response.message}`));
+        this.docObservableMap.delete(docId);
         return of({
-            id: DocFetchErrorId,
-            content: docFetchErrorContent(id),
+            id: docErrorId,
+            content: docErrorContent(docId),
         });
     }
 
 }
 
-function encodeToLowerCase(str: string): string {
-    return str.replace(/[A-Z_]/g, char => char.toLowerCase() + '_');
+/**
+ * 转换文档id，在大写字母和`_`后面添加`_`，并把大写字母转换为小写字母
+ *
+ * @param docId 文档id
+ * @return 转换后的文档id
+ */
+function convertDocId(docId: string): string {
+    return docId.replace(/[A-Z_]/g, char => char.toLowerCase() + '_');
 }
